3aefaba104e892daeaf031ab3cf1018a11b9a4b5
bitturana-ab <b2ra830@gmail.com>
2025-07-11 22:57:13 +0530

    process exit event loop

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 console.log("first line");
 const http = require("http");
 
+const port = 3000;
+
 http
   .createServer((req, res) => {
-    console.log("req: " + req);
     console.log(req);
+    // stop server and exit event loop on req
+    process.exit();
   })
-  .listen(3000);
+  .listen(port, () => {
+    console.log(`server is running on port ${port}`);
+  });
