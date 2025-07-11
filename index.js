f5f38c9a54ebc8326f92525b9eadb00ce4ebfa2f
bitturana-ab <b2ra830@gmail.com>
2025-07-11 23:04:55 +0530

    req.url method get or post

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const http = require("http");
 
 const port = 3000;
 
+// http
+//   .createServer((req, res) => {
+//     console.log(req);
+//     // stop server and exit event loop on req
+//     process.exit();
+//   })
+//   .listen(port, () => {
+//     console.log(`server is running on port ${port}`);
+//   });
+
 http
   .createServer((req, res) => {
-    console.log(req);
-    // stop server and exit event loop on req
-    process.exit();
+    console.log(req.url, req.method, req.headers);
   })
   .listen(port, () => {
     console.log(`server is running on port ${port}`);
