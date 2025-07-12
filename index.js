b3bccd620e883d5d67fceb18b619ce6235a5c1b2
bitturana-ab <b2ra830@gmail.com>
2025-07-12 17:50:19 +0530

    response as html page

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ const port = 3000;
 http
   .createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    // send response
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+
+    res.write("<head>Html response</head>");
+    res.write("<body><h1>Response of nodejs</h1></body>");
+    res.write("</html>");
+    res.end(); // end the response
   })
   .listen(port, () => {
     console.log(`server is running on port ${port}`);
