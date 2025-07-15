c2387e713409a9e5db990340297a55a4f8ec33c2
bitturana-ab <b2ra830@gmail.com>
2025-07-15 23:28:39 +0530

    use get and post method for req url

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ app.use("/", (req, res, next) => {
   console.log("first middleware");
   next();
 });
-
-app.use("/ab", (req, res, next) => {
-  console.log("second middleware");
-  res.send("<h1>Hello from Express!</h1>");
+// get request hanler
+app.get("/contact-us", (req, res, next) => {
+  console.log("contact-us middleware");
+  res.send(`<h1>
+    Contact us!
+    </h1>
+    <p>Fill out the form below to get in touch.</p>
+    <form action="/contact-us" method="POST">
+      <input type="text" name="name" placeholder="Your Name" required>
+      <input type="password" name="password" placeholder="Your Password" required>
+      <button type="submit">Submit</button>
+    </form>
+    `);
+});
+// post or submit form handler
+app.post("/contact-us", (req, res, next) => {
+  console.log("contact-us post middleware");
+  res.send(`<h1>Thank you for contacting us!</h1>`);
 });
 
 app.listen(port, () => {
