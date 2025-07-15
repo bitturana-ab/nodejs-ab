b2878b437ff3ba5d39167e29b92a851f1a737045
bitturana-ab <b2ra830@gmail.com>
2025-07-15 22:44:39 +0530

    use express frmwrk for easily create server

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-console.log("first line");
 const http = require("http");
+const express = require("express");
+
+const app = express();
 
 const port = 3000;
 
-// http
-//   .createServer((req, res) => {
-//     console.log(req);
-//     // stop server and exit event loop on req
-//     process.exit();
-//   })
-//   .listen(port, () => {
-//     console.log(`server is running on port ${port}`);
-//   });
+app.use((req, res, next) => {
+  console.log("first middleware");
+  next();
+});
 
-http
-  .createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // send response
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
+app.use((req, res, next) => {
+  console.log("second middleware");
+  res.send("<h1>Hello from Express!</h1>");
+});
 
-    res.write("<head>Html response</head>");
-
-    if (req.url === "/ab") {
-      res.write("<body><h2>ab page</h2></body>");
-    } else if (req.url === "/home") {
-      res.write("<body><h2>home page</h2></body>");
-    } else {
-      res.write("<body><h1>Response of nodejs</h1></body>");
-    }
-    res.write("</html>");
-    res.end(); // end the response
-  })
-  .listen(port, () => {
-    console.log(`server is running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
